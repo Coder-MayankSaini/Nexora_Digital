56b6cbc08c4b8fffd9c0f09c69aab9033d2392b9
Mannat <officialmail.mayanksaini@gmail.com>
2025-06-12 13:46:02 +0530

    Fix hydration error on login page by updating HydrationFix component

--- components/HydrationFix.tsx
+++ components/HydrationFix.tsx
@@ -3,14 +3,19 @@
 import { useEffect } from 'react';
 
 /**
- * This component fixes hydration errors related to the 'hydrated' class
- * by ensuring the class is only added/removed on the client side
+ * This component fixes hydration errors related to class and attribute mismatches
+ * by ensuring they are only handled on the client side
  */
 export default function HydrationFix() {
   useEffect(() => {
     // Remove the 'hydrated' class if it exists
     if (document.documentElement.classList.contains('hydrated')) {
       document.documentElement.classList.remove('hydrated');
+    }
+    
+    // Remove the 'cz-shortcut-listen' attribute if it exists
+    if (document.body.hasAttribute('cz-shortcut-listen')) {
+      document.body.removeAttribute('cz-shortcut-listen');
     }
   }, []);
 
